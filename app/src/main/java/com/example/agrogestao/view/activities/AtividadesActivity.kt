576405770dce7e3bedbf2e2ba923b95f37db3673
famosofk <fabianogmjunior@gmail.com>
2020-05-27 11:21:21 -0300

    criando a dialog pra criação de turmas/fazendas/programas

--- app/src/main/java/com/example/agrogestao/view/activities/AtividadesActivity.kt
+++ app/src/main/java/com/example/agrogestao/view/activities/AtividadesActivity.kt
@@ -1,8 +1,11 @@
 package com.example.agrogestao.view.activities
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.example.agrogestao.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AtividadesActivity : AppCompatActivity() {
 
@@ -11,9 +14,27 @@ class AtividadesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_atividades)
 
 
-        /*
-        * Temos que fazer a listagem das atividades que foram cadastradas
-        * colocar um Floating que permita cadastrar mais.
-        * */
+        val fab: FloatingActionButton = findViewById(R.id.floatingActionButton)
+        fab.setOnClickListener { view ->
+            createAlertDialog("teste")
+        }
+    }
+
+    private fun createAlertDialog(title: String) {
+        val mDialogView =
+            LayoutInflater.from(this).inflate(R.layout.cadastro_programa_fazenda, null)
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle(title)
+            .create()
+        mBuilder.show()
+
+        /*  cadastrarDialogButton.setOnClickListener{
+              mBuilder.dismiss()
+              val name = mDialogView.editNomeDialog.text.toString()
+              val complemento = mDialogView.editComplementoText.text.toString()
+              Toast.makeText(this, "" + name+complemento, Toast.LENGTH_SHORT).show()
+          } */
+
     }
 }
--- app/src/main/java/com/example/agrogestao/view/activities/NavigationActivity.kt
+++ app/src/main/java/com/example/agrogestao/view/activities/NavigationActivity.kt
@@ -11,9 +11,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.agrogestao.R
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 
 class NavigationActivity : AppCompatActivity() {
 
@@ -25,11 +23,11 @@ class NavigationActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        /*  val fab: FloatingActionButton = findViewById(R.id.fab)
+          fab.setOnClickListener { view ->
+              Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                  .setAction("Action", null).show()
+          } */
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
