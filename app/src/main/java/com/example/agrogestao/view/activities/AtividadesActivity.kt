0f8fc8a623ab7ed54d0291b83add9522b6c847cf
famosofk <fabianogmjunior@gmail.com>
2020-05-27 11:50:08 -0300

    criando a dialog pra criação de turmas/fazendas/programas

--- app/src/main/java/com/example/agrogestao/view/activities/AtividadesActivity.kt
+++ app/src/main/java/com/example/agrogestao/view/activities/AtividadesActivity.kt
@@ -3,9 +3,11 @@ package com.example.agrogestao.view.activities
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.agrogestao.R
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.cadastro_programa_fazenda.view.*
 
 class AtividadesActivity : AppCompatActivity() {
 
@@ -14,27 +16,31 @@ class AtividadesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_atividades)
 
 
-        val fab: FloatingActionButton = findViewById(R.id.floatingActionButton)
-        fab.setOnClickListener { view ->
-            createAlertDialog("teste")
-        }
+        val fabPrograma: com.github.clans.fab.FloatingActionButton =
+            findViewById(R.id.fabAddPrograma)
+        fabPrograma.setOnClickListener { view -> createAlertDialog("Criar programa") }
+        val fabFazenda: com.github.clans.fab.FloatingActionButton = findViewById(R.id.fabAddFazenda)
+        fabFazenda.setOnClickListener { view -> createAlertDialog("Criar fazenda") }
+        val fabAtividade: com.github.clans.fab.FloatingActionButton =
+            findViewById(R.id.fabAddAtividade)
+        fabAtividade.setOnClickListener { view -> createAlertDialog("Criar atividade") }
     }
 
     private fun createAlertDialog(title: String) {
         val mDialogView =
             LayoutInflater.from(this).inflate(R.layout.cadastro_programa_fazenda, null)
+        val cadastrarButton: Button = mDialogView.findViewById(R.id.cadastrarDialogButton)
         val mBuilder = AlertDialog.Builder(this)
             .setView(mDialogView)
             .setTitle(title)
             .create()
         mBuilder.show()
-
-        /*  cadastrarDialogButton.setOnClickListener{
+        cadastrarButton.setOnClickListener {
               mBuilder.dismiss()
               val name = mDialogView.editNomeDialog.text.toString()
               val complemento = mDialogView.editComplementoText.text.toString()
               Toast.makeText(this, "" + name+complemento, Toast.LENGTH_SHORT).show()
-          } */
+        }
 
     }
 }
