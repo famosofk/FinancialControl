2dd3380c8f25b57069bdc928e60b7e14e66b4313
famosofk <fabianogmjunior@gmail.com>
2020-06-07 12:28:04 -0300

    Recuperando todas as fazendas e colocando em uma lista

--- app/src/main/java/com/example/agrogestao/view/viewholder/FarmsViewHolder.kt
+++ app/src/main/java/com/example/agrogestao/view/viewholder/FarmsViewHolder.kt
@@ -1,13 +1,19 @@
 package com.example.agrogestao.view.viewholder
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.agrogestao.R
 import com.example.agrogestao.models.Farm
 
 class FarmsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(farm: Farm) {
+        val fazendaText: TextView = itemView.findViewById(R.id.farmTextViewListagem)
+        val programaText: TextView = itemView.findViewById(R.id.programaTextViewListagem)
 
+        fazendaText.setText(farm.codigoFazenda)
+        fazendaText.setText(farm.programa)
     }
 
 
