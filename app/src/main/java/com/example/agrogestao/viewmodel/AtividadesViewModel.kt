605fbeadb9d1f1eabc5650bc878e34b4859aea92
famosofk <fabianogmjunior@gmail.com>
2020-06-07 10:01:08 -0300

    Recuperando todas as fazendas e colocando em uma lista

---
+++ app/src/main/java/com/example/agrogestao/viewmodel/AtividadesViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.agrogestao.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.agrogestao.models.Farm
+import io.realm.Realm
+import io.realm.kotlin.where
+
+class AtividadesViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mFarms = MutableLiveData<List<Farm>>()
+    val farmtList: LiveData<List<Farm>> = mFarms
+
+    fun load(filter: Int) {
+        val list = mutableListOf<Farm>()
+        val realm: Realm = Realm.getDefaultInstance()
+        val query = realm.where<Farm>().findAll()
+        list.addAll(query)
+        mFarms.value = list
+
+
+    }
+
+
+}
