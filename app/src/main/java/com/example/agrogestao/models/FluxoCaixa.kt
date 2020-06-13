0b441f26e26423bf063a8b393c2d6322aae73704
famosofk <fabianogmjunior@gmail.com>
2020-06-13 13:17:14 -0300

    criação do fluxo de caixa

--- app/src/main/java/com/example/agrogestao/models/FluxoCaixa.kt
+++ app/src/main/java/com/example/agrogestao/models/FluxoCaixa.kt
@@ -1,9 +1,12 @@
 package com.example.agrogestao.models
 
+import io.realm.RealmList
 import io.realm.RealmObject
 
-open class FluxoCaixa(var farm: Farm? = null) : RealmObject(){
+open class FluxoCaixa() : RealmObject() {
 
+    var farm = ""
+    var list = RealmList<ItemFluxoCaixa>()
 
 
 }
